182f2c9f5e1b4175958739e0928c1b2b992c7d10
William Y Wong <wwong1987@gmail.com>
2019-04-15 22:14:53 -0700

    working on cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'https://petful.dc5will.now.sh' || 'http://localhost:3000',
 }
