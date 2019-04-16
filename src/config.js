70211e81ef6925d1083c89c4c5fb286ee2224439
William Y Wong <wwong1987@gmail.com>
2019-04-15 22:36:34 -0700

    this should resolve cors issues

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: 'https://petful-client.dc5will.now.sh',
   NODE_ENV: process.env.NODE_ENV || 'development',
 }
